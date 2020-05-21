b1a5db39eb0707681cfd8a975a1863b0e3386942
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2020-05-20 23:25:17 -0300

    Adiciona fluxo de login

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import inicio, grupo
+from .views import inicio, logar, deslogar, grupo
 
 urlpatterns = [
     path('', inicio, name='inicio'),
+
+    path('login', logar, name='login'),
+    path('logout', deslogar, name='logout'),
+
     path('<grupo>', grupo, name='grupo'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import Http404
 from .models import Grupo
 
 def inicio(request):
@@ -6,6 +12,37 @@ def inicio(request):
     contexto = {'grupos': grupos}
 
     return render(request, 'inicio.html', contexto) 
+
+def logar(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
+        else:
+            return render(request, 'administracao/login.html', {})
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+
+        if '@' in email:
+            email = User.objects.get(email=email).username
+
+        usuario = authenticate(request, username=email, password=senha)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/ciaten')
+        else:
+            messages.warning(request, 'Usuário e/ou senha incorretos. Por favor, tente novamente.')
+            contexto = {'email': email, 'mensagem': 'Usuário e/ou senha incorretos. Por favor, tente novamente.'}
+    else:
+        raise Http404('Método de requisição não aceito')
+
+@login_required(login_url='/login')
+def deslogar(request):
+    logout(request)
+    
+    return redirect('/')
 
 def grupo(request, grupo):
     grupo = get_object_or_404(Grupo, sigla=grupo)
